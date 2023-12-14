c91c65081d261cb88369a0d0aa1c3d1decf2e9fc
xldyswa <145743621+xldyswa@users.noreply.github.com>
2023-12-14 17:31:51 +0800

    Update curand.cu

--- curand/curand.cu
+++ curand/curand.cu
@@ -4,25 +4,44 @@
 #include <stdlib.h>
 #include <curand_kernel.h>
 #include <curand.h>
+#include <ctime>
 
 extern "C"
 {
-    __declspec(dllexport) void generate_random_numbers(float* hostData, int n);
-
+    __declspec(dllexport) void generate_uniform_random(float* hostData, int n);
+    __declspec(dllexport) void generate_normal_random(float* hostData, int n, float mean, float stddev);
+    __declspec(dllexport) void generate_poisson_random(unsigned int* hostData, int n, double lambda);
 }
+
 __global__ void setup_kernel(curandState* state, unsigned long seed) {
     int id = threadIdx.x + blockIdx.x * blockDim.x;
     curand_init(seed, id, 0, &state[id]);
 }
 
-__global__ void generate_kernel(curandState* state, float* data, int n) {
+__global__ void generate_uniform_kernel(curandState* state, float* data, int n) {
     int id = threadIdx.x + blockIdx.x * blockDim.x;
     if (id < n) {
         data[id] = curand_uniform(&state[id]);
     }
 }
 
-void generate_random_numbers(float* hostData, int n) {
+__global__ void generate_normal_kernel(curandState* state, float* data, int n, float mean, float stddev) {
+    int id = threadIdx.x + blockIdx.x * blockDim.x;
+    if (id < n) {
+        data[id] = curand_normal(&state[id]) * stddev + mean;
+    }
+}
+
+__global__ void generate_poisson_kernel(curandState* state, unsigned int* data, int n, double lambda) {
+    int id = threadIdx.x + blockIdx.x * blockDim.x;
+    if (id < n) {
+        data[id] = curand_poisson(&state[id], lambda);
+    }
+}
+
+// Wrapper functions to be called from C#
+
+void generate_uniform_random(float* hostData, int n) {
     curandState* devStates;
     float* devData;
 
@@ -36,12 +55,44 @@ void generate_random_numbers(float* hostData, int n) {
     setup_kernel << <(n + 255) / 256, 256 >> > (devStates, time(NULL));
 
     // Generate random numbers
-    generate_kernel << <(n + 255) / 256, 256 >> > (devStates, devData, n);
+    generate_uniform_kernel << <(n + 255) / 256, 256 >> > (devStates, devData, n);
 
     // Copy results to host
     cudaMemcpy(hostData, devData, n * sizeof(float), cudaMemcpyDeviceToHost);
 
     // Cleanup
+    cudaFree(devStates);
+    cudaFree(devData);
+}
+
+void generate_normal_random(float* hostData, int n, float mean, float stddev) {
+    curandState* devStates;
+    float* devData;
+
+    cudaMalloc((void**)&devStates, n * sizeof(curandState));
+    cudaMalloc((void**)&devData, n * sizeof(float));
+
+    setup_kernel << <(n + 255) / 256, 256 >> > (devStates, time(NULL));
+    generate_normal_kernel << <(n + 255) / 256, 256 >> > (devStates, devData, n, mean, stddev);
+
+    cudaMemcpy(hostData, devData, n * sizeof(float), cudaMemcpyDeviceToHost);
+
+    cudaFree(devStates);
+    cudaFree(devData);
+}
+
+void generate_poisson_random(unsigned int* hostData, int n, double lambda) {
+    curandState* devStates;
+    unsigned int* devData;
+
+    cudaMalloc((void**)&devStates, n * sizeof(curandState));
+    cudaMalloc((void**)&devData, n * sizeof(unsigned int));
+
+    setup_kernel << <(n + 255) / 256, 256 >> > (devStates, time(NULL));
+    generate_poisson_kernel << <(n + 255) / 256, 256 >> > (devStates, devData, n, lambda);
+
+    cudaMemcpy(hostData, devData, n * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+
     cudaFree(devStates);
     cudaFree(devData);
 }
